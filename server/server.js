d8b91b30c32a6873c8dbf2f20b5d3f1508bc66a0
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2025-12-13 15:08:45 +0530

    Initial Setup for the project done

--- server/server.js
+++ server/server.js
@@ -10,10 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/gridlayout', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/gridlayout')
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
