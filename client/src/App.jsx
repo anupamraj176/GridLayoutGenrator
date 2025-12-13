7345ab1037c7befea316ce9c0a153faa25c6e0ae
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2025-12-13 15:15:21 +0530

    tailwind setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
         Hello World
       </h1>
